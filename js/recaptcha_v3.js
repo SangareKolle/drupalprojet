f05602fed028697e0f582d3c0ad84012e5652cdd
dench0 <dench0@yandex.ru>
2018-11-16 20:53:51 +0200

    Development version for testing.

---
+++ js/recaptcha_v3.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+  /**
+   * recaptcha v3 js.
+   */
+  Drupal.behaviors.reCaptchaV3 = {
+    attach: function (context, settings) {
+      $('input[data-recaptcha-v3-sitekey]', context).once('recaptcha-v3').each(function () {
+        var $input = $(this);
+        grecaptcha.ready(function () {
+          grecaptcha.execute(
+            $input.data('recaptchaV3Sitekey'),
+            {
+              action: $input.data('recaptchaV3Action')
+            })
+          .then(function (token) {
+            $input.val(token);
+          });
+        });
+      });
+    }
+  };
+
+})(jQuery);
