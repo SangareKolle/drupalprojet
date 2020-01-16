9efdb1bfb48876879d974f9860db16d7e971a4d4
Majid Ali Khan <majid-a@live.com>
2020-01-16 21:02:34 +0100

    Issue #3106907: Remove action from captcha points form before removing the action entity

--- src/Form/ReCaptchaV3ActionDeleteForm.php
+++ src/Form/ReCaptchaV3ActionDeleteForm.php
@@ -15,9 +15,26 @@ class ReCaptchaV3ActionDeleteForm extends EntityDeleteForm {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    // @todo need to find count of captcha points using deleted action and show appropriate title
-    // @see \Drupal\node\Form\NodeTypeDeleteConfirm::buildForm()
-    return parent::buildForm($form, $form_state);
+    $action = $this->entityTypeManager->getStorage('captcha_point')->getQuery()
+      ->condition('captchaType', 'recaptcha_v3/' . $this->entity->id())
+      ->execute();
+    if (!empty($action)) {
+      $caption = '<p>' . $this->formatPlural(
+          count($action),
+          '%label is used by 1 captcha point form on your site. You can not remove %label until you have removed it from %formId captcha points form.',
+          '%label is used by @count captcha point forms on your site. You may not remove %label until you have removed %label from %formId.',
+          [
+            '%label' => $this->entity->label(),
+            '%formId' => implode(", ", $action),
+          ]
+        ) . '</p>';
+      $form['description'] = ['#markup' => $caption];
+      return $form;
+    }
+    else {
+      return parent::buildForm($form, $form_state);
+    }
+    // @todo needs to do same as above in case of recaptcha v3 action being used in webform.
   }
 
 }
