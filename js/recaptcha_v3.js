62ae0b1499f2d42375006fe3eece28327254fbb5
mnico <mnico@1119544.no-reply.drupal.org>
2020-10-27 11:28:04 +0200

    Issue #3133107 by PrineShazar, mnico, dench0: async loading causes grecaptcha is not defined error (D7)

--- js/recaptcha_v3.js
+++ js/recaptcha_v3.js
@@ -7,16 +7,22 @@
     attach: function (context, settings) {
       $('input[data-recaptcha-v3-sitekey]', context).once('recaptcha-v3').each(function () {
         var $input = $(this);
-        grecaptcha.ready(function () {
-          grecaptcha.execute(
-            $input.data('recaptcha-v3-sitekey'),
-            {
-              action: $input.data('recaptcha-v3-action')
-            })
-          .then(function (token) {
-            $input.val(token);
-          });
-        });
+        var timer = setInterval(function () {
+          if (typeof grecaptcha !== 'undefined') {
+            grecaptcha.ready(function () {
+              grecaptcha.execute(
+                $input.data('recaptcha-v3-sitekey'),
+                {
+                  action: $input.data('recaptcha-v3-action')
+                })
+                .then(function (token) {
+                  $input.val(token);
+                  $input.trigger('change');
+                });
+            });
+            clearInterval(timer);
+          }
+        }, 500);
       });
     }
   };
